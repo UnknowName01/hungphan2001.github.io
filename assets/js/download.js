8b6ee6bae2591ed7cb520f9947b04506a81dcd93
huy29012001 <huy29012001@gmail.com>
2021-03-25 11:07:15 +0700

    add js

---
+++ assets/js/download.js
@@ -0,0 +1,7 @@
+document.getElementById("btnAsus").onclick = function () {
+    document.getElementById("xiaomi").style.display = 'none';
+};
+
+document.getElementById("btnXiaomi").onclick = function () {
+    document.getElementById("xiaomi").style.display = '';
+};
